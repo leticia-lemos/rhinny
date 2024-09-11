4c285678540991ae26dbd172d7432ea85d369e5d
BlackoutMomo <sergiogabriel2511g@gmail.com>
2024-09-11 12:43:14 -0300

    firebase-iniciado

--- js/register.js
+++ js/register.js
@@ -1,22 +1,16 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAK1Y4DAftt63mVx3tngghMWXYDfg5zzTo",
-    authDomain: "rhinny-319e6.firebaseapp.com",
-    projectId: "rhinny-319e6",
-    storageBucket: "rhinny-319e6.appspot.com",
-    messagingSenderId: "860506842460",
-    appId: "1:860506842460:web:e3176d1e88d16664590d99",
-    measurementId: "G-KKE67JZ0M6"
-}
 
-firebase.auth().createUserWithEmailAndPassword(email, password)
+firebase.auth().createUserWithEmailAndPassword("receba@gmail.com", "oieeeeeeeee")
   .then((userCredential) => {
     // Signed in 
     var user = userCredential.user;
+    console.log('entroooo')
     // ...
   })
   .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
+    console.log(errorCode)
+    console.log(errorMessage)
     // ..
   });
 
