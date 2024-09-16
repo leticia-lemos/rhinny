319c84c3968383ddb023b28252d444f0193dda54
Blackout <sergiogabriel2606g@outlook.com>
2024-09-16 13:32:40 -0300

    aiii meu

--- firebase_js/mostrarDados.js
+++ firebase_js/mostrarDados.js
@@ -1,31 +1,31 @@
 
-// let globalUserId = null;
+ let globalUserId = null;
 
-// firebase.auth().onAuthStateChanged((user) => {
-//   if (user) {
-//     globalUserId = user.uid;
-//     console.log('usuario logado'+ globalUserId)
-//   } else {
-//     console.log("Nenhum usuário logado.");
-//   }
-// });
+ firebase.auth().onAuthStateChanged((user) => {
+   if (user) {
+     globalUserId = user.uid;
+     console.log('usuario logado'+ globalUserId)
+   } else {
+     console.log("Nenhum usuário logado.");
+   }
+ });
 
-// function getUserData(globalUserId) {
-//     const userRef = firebase.firestore().collection('users').doc(globalUserId); // Supondo que a coleção se chama 'users'
-//     userRef.get().then((doc) => {
-//         if (doc.exists) {
-//             const userData = doc.data();
-//             displayUserData(userData);
-//         } else {
-//             console.log("Nenhum documento encontrado!");
-//         }
-//     }).catch((error) => {
-//         console.error("Erro ao obter documento:", error);
-//     });
-// }
+ function getUserData(globalUserId) {
+     const userRef = firebase.firestore().collection('users').doc(globalUserId); // Supondo que a coleção se chama 'users'
+     userRef.get().then((doc) => {
+         if (doc.exists) {
+             const userData = doc.data();
+             displayUserData(userData);
+         } else {
+             console.log("Nenhum documento encontrado!");
+         }
+     }).catch((error) => {
+         console.error("Erro ao obter documento:", error);
+     });
+ }
 
-// // Função para exibir dados do usuário na tela
-// function displayUserData(data) {
-//     console.log(data.uid)
-// }
+ // Função para exibir dados do usuário na tela
+ function displayUserData(data) {
+     console.log(data.uid)
+ }
   
