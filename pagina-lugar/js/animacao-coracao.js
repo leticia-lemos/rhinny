56e2750537edda0dfe193f2b14fbe680068865c1
Blackout <sergiogabriel2606g@outlook.com>
2024-11-28 20:34:36 -0300

    tudo funcionando

--- pagina-lugar/js/animacao-coracao.js
+++ pagina-lugar/js/animacao-coracao.js
@@ -1,6 +1,22 @@
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    var uid = user.uid;
+    const favoritoRef = db.collection("users").doc(uid).collection('favoritos');
+    favoritoRef.doc(url).get().then((doc) => {
+      if(doc.exists){
+        heartIcon.classList.add('filled');
+        heartIcon.src = './img/Heart-filled.svg';        
+      }else{  
+        heartIcon.classList.remove('filled');
+        heartIcon.src = './img/Heart.svg'; // Muda a imagem para o coração não preenchido
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
+  }})
+
 // Seleciona o ícone de coração e a imagem do ícone
-const heartIcon = document.querySelector('.heart-icon');
-const heartImg = heartIcon.querySelector('img');
+const heartIcon = document.querySelector('#favoritar');
 
 // Adiciona um evento de clique ao ícone de coração
 heartIcon.addEventListener('click', () => {
@@ -8,10 +24,72 @@ heartIcon.addEventListener('click', () => {
   if (heartIcon.classList.contains('filled')) {
     // Se já estiver preenchido, remove a classe 'filled'
     heartIcon.classList.remove('filled');
-    heartImg.src = './img/Heart.svg'; // Muda a imagem para o coração não preenchido
+    heartIcon.src = './img/Heart.svg'; // Muda a imagem para o coração não preenchido
+    document
+    .getElementsByClassName("heart-icon")[0]
+    .addEventListener("click", () => {
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          var uid = user.uid;
+          const favoritoRef = db.collection("users").doc(uid).collection('favoritos');
+          favoritoRef.doc(url).get().then((doc) => {
+            if(doc.exists){
+              favoritoRef.doc(url).delete()
+              .then(() => {
+                console.log("tirado do favorito");
+              })
+              .catch((error) => {
+                console.log(error);
+              })
+            }else{  
+              console.log('nao ta no favorito')
+            }
+          }).catch((error) => {
+            console.log(error)
+          })
+        }
+      })
+      //
+      ;
+    });
   } else {
     // Se não estiver preenchido, adiciona a classe 'filled'
     heartIcon.classList.add('filled');
-    heartImg.src = './img/Heart-filled.svg'; // Muda a imagem para o coração preenchido
-  }
-});
+    heartIcon.src = './img/Heart-filled.svg';
+    var nomeLugar = localStorage.getItem("nomeLugar");
+      var photoLugar = localStorage.getItem("img");
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          var uid = user.uid;
+          const favoritoRef = db
+            .collection("users")
+            .doc(uid)
+            .collection("favoritos");
+          favoritoRef
+            .doc(url)
+            .get()
+            .then((doc) => {
+              if (doc.exists) {
+                console.log(" ja ta la");
+              } else {
+                favoritoRef
+                  .doc(url)
+                  .set({
+                    photos: photoLugar,
+                    name: nomeLugar,
+                    id: url,
+                  })
+                  .then(() => {
+                    console.log("Favoritado");
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                  });
+              }
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      })
+    }});
